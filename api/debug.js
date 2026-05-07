cba1c285af746bd61d48ff48cc7313f36b29b51e
alimuyisa6 <alimuyisa6@gmail.com>
2026-05-07 18:51:48 +0300

    Delete api directory

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  res.status(200).json({
-    supabaseUrl: process.env.SUPABASE_URL || 'MISSING',
-    keyExists: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-    keyLength: (process.env.SUPABASE_SERVICE_ROLE_KEY || '').length,
-    keyFirst10: (process.env.SUPABASE_SERVICE_ROLE_KEY || '').slice(0, 10)
-  });
-}
